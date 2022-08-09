23682e454dc1ed1c77566e7a412f6cdf5f1bf9e2
somtonu <onusomtochukwusamuel@gmail.com>
2022-08-10 00:01:51 +0100

    header file update

--- main.h
+++ main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <stdlib.h>
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_string(char *);
 int (*get_print(char s))(va_list, flags_t *);
 int get_flag(char s, flags_t *f);
+
 #endif /* MAIN_H */
